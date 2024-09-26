e6f62017b57a9a893e8f8f79f0e0872b5fc5257c
nailasalmayusroini <nailasalmayusroini@mail.ugm.ac.id>
2024-09-26 16:03:15 +0000

    64

--- Problem1/sum_numbersTest.cpp
+++ Problem1/sum_numbersTest.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int n = 6;
+    int n = 64;
 
     int sum = 0;
 
@@ -13,9 +13,9 @@ int main() {
     }
 
     string expectedResult[3] = {
-        "Input: 6",
-        "Output: Sum = 21",
-        "(Explanation: 1 + 2 + 3 + 4 + 5 + 6 = 21 )"
+        "Input: 64",
+        "Output: Sum = 2080",
+        "(Explanation: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20 + 21 + 22 + 23 + 24 + 25 + 26 + 27 + 28 + 29 + 30 + 31 + 32 + 33 + 34 + 35 + 36 + 37 + 38 + 39 + 40 + 41 + 42 + 43 + 44 + 45 + 46 + 47 + 48 + 49 + 50 + 51 + 52 + 53 + 54 + 55 + 56 + 57 + 58 + 59 + 60 + 61 + 62 + 63 + 64 = 2080 )"
     };
     
     string actualResult[3] = {
